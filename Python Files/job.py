7cb805c25970f1fd39238468a14b7852ce9a14d5
lukebrown7-svg <7lukebrown7@gmail.com>
2025-12-08 10:58:03 +0000

    Added job.py and job_manager.py templates to the project

---
+++ Python Files/job.py
@@ -0,0 +1,31 @@
+class Job:
+
+    def __init__(self, name, category, rate, date, hours):
+        pass
+
+    def get_name(self):
+        pass
+
+    def get_category(self):
+        pass
+
+    def get_rate(self):
+        pass
+
+    def get_date(self):
+        pass
+
+    def get_hours(self):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __hash__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
