c5efaf152df8114b80cf89f57e02f3af03ac808e
lukebrown7-svg <7lukebrown7@gmail.com>
2025-12-08 11:03:28 +0000

    Added the job class constructor with private attributes

--- Python Files/job.py
+++ Python Files/job.py
@@ -1,6 +1,11 @@
 class Job:
 
     def __init__(self, name, category, rate, date, hours):
+        self._name = name
+        self._category = category
+        self._rate = rate
+        self._date = date
+        self._hours = hours
         pass
 
     def get_name(self):
