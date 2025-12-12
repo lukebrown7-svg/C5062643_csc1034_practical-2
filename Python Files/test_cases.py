0b5adf874e457bcc32f1ed01f3bb905f7292a28d
lukebrown7-svg <7lukebrown7@gmail.com>
2025-12-12 01:18:40 +0000

    Added Boundary Case Test

--- Python Files/test_cases.py
+++ Python Files/test_cases.py
@@ -7,9 +7,15 @@ job1 = Job("Luke","Student",10.0,"12/12/12",2)
 print(jm.add_job(job1))
 print(jm.get_jobs())
 
-print("Boundary Case Test 1")
+print("Erroneous Case Test 1")
 job2 = Job("Luke","Student",10.0,"12/12/12",7)
 print(jm.add_job(job2))
+
+print("Boundary Case Test 1")
+job2 = Job("Luke","Student",10.0,"12/12/12",6)
+print(jm.add_job(job2))
+
+
 
 
 
