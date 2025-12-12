78efc39ab2361b157ba8dfb75a37cee06c846ce8
lukebrown7-svg <7lukebrown7@gmail.com>
2025-12-12 01:22:13 +0000

    Added Exceptional Case Test

--- Python Files/test_cases.py
+++ Python Files/test_cases.py
@@ -5,15 +5,19 @@ print("Normal Case Test")
 jm = JobManager()
 job1 = Job("Luke","Student",10.0,"12/12/12",2)
 print(jm.add_job(job1))
-print(jm.get_jobs())
 
 print("Erroneous Case Test 1")
 job2 = Job("Luke","Student",10.0,"12/12/12",7)
 print(jm.add_job(job2))
 
 print("Boundary Case Test 1")
-job3 = Job("Luke","Student",10.0,"12/12/12",6)
+job3 = Job("James","Student",10.0,"12/12/12",6)
 print(jm.add_job(job3))
+print(jm.get_jobs())
+
+print("Exceptional Case Test")
+job4 = Job("Joe","Teacher","notanumber","12/12/12","notanumber")
+print(jm.add_job(job4))
 
 
 
